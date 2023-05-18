285ae110ee3300d4b1cb3d15c2f3dfdea7cbb4eb
danielfariashsl <119943286+danielfariashsl@users.noreply.github.com>
2023-05-18 15:01:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
       chunks = text_splitter.split_text(text)
       
        # create embeddings
-      embeddings = OpenAIEmbeddings()
+      embeddings = OpenAIEmbeddings(openai_api_key="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")
       knowledge_base = FAISS.from_texts(chunks, embeddings)
       
       # show user input
